49cc3b3b4b360dde38b912d43886786fa72047af
Tharii9 <thariieranga2002@gmail.com>
2024-05-10 23:19:03 +0530

    UI designs

---
+++ app/src/main/java/com/example/budgetbuddy/AddExpenses.kt
@@ -0,0 +1,16 @@
+package com.example.budgetbuddy
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class AddExpenses : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_add_expenses)
+
+    }
+}
